f9ffd8225235a12c90547ddc498b0cde4b0867a7
Roque Javier Ducuara <rducuara@everis.com>
2020-03-09 23:15:52 +0100

    Adición de archivos de variables de entorno para nodo cliente

---
+++ CreateFabric/env_vars.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export STACKNAME=fullhybrid-amb
+export NETWORKNAME=fullhybrid
+export NETWORKID=n-DWZKXSKIHNFEFGESWDEW4WS3UI
+export VPCENDPOINTSERVICENAME=com.amazonaws.us-east-1.managedblockchain.n-dwzkxskihnfefgeswdew4ws3ui
+export MEMBERNAME=member1
+export NETWORKVERSION=1.2
+export ADMINUSER=admin
+export ADMINPWD=AdminpwdFullHybrid1!
+export MEMBERID=m-Y46UN7OBKZB63HDNQIZNFHVSY4
+export ORDERINGSERVICEENDPOINT=orderer.n-dwzkxskihnfefgeswdew4ws3ui.managedblockchain.us-east-1.amazonaws.com:30001
+export ORDERINGSERVICEENDPOINTNOPORT=${ORDERINGSERVICEENDPOINT::-6}
+export CAENDPOINT=ca.m-y46un7obkzb63hdnqiznfhvsy4.n-dwzkxskihnfefgeswdew4ws3ui.managedblockchain.us-east-1.amazonaws.com:30002
+export NODEID=nd-KVMKWKXD6VCSRG3XX24U5V5MTE
+export PEERENDPOINT=nd-kvmkwkxd6vcsrg3xx24u5v5mte.m-y46un7obkzb63hdnqiznfhvsy4.n-dwzkxskihnfefgeswdew4ws3ui.managedblockchain.us-east-1.amazonaws.com:30003
+export PEERSERVICEENDPOINTNOPORT=${PEERENDPOINT::-6}
+export PEEREVENTENDPOINT=nd-kvmkwkxd6vcsrg3xx24u5v5mte.m-y46un7obkzb63hdnqiznfhvsy4.n-dwzkxskihnfefgeswdew4ws3ui.managedblockchain.us-east-1.amazonaws.com:30004
+
+
+export MSP_PATH=/opt/home/admin-msp
+export MSP=$MEMBERID
+export ORDERER=$ORDERINGSERVICEENDPOINT
+export PEER=$PEERENDPOINT
+export CHANNEL=mychannel
+export CAFILE=/opt/home/managedblockchain-tls-chain.pem
+export CHAINCODENAME=mycc
+export CHAINCODEVERSION=v0
+export CHAINCODEDIR=github.com/chaincode_example02/go
