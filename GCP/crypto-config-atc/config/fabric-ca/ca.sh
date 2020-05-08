3b49410d78e83318680a36cadfaf42809daa1acf
Roque Javier Ducuara Sosa <rducuara@everis.com>
2020-05-08 10:52:59 -0500

    Add config for fabric-ca

---
+++ GCP/crypto-config-atc/config/fabric-ca/ca.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+### Script para inicializar un server de Fabric-CA sin necesidad de contenedores Docker
+
+## Crea usuario fabric
+sudo useradd fabric
+## Asigna permisos de sudo al usuario
+sudo usermod -aG sudo fabric
+## inicia sesión con usuario fabric
+su - fabric
+## instalar GO
+sudo apt install golang
+## Configura las variables de entorno para GO
+echo 'export GOPATH=$HOME/go' | tee -a ~/.bashrc
+echo 'export GOBIN=$GOPATH/bin' | tee -a ~/.bashrc
+source ~/.bashrc
+# Descarga los ejemplos de fabric y los binarios
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- -d
+# copia los binarios al path del sistema
+sudo cp ~/fabric-samples/bin/* /usr/local/bin/
+# Descarga Fabric-CA binarios
+go get -u github.com/hyperledger/fabric-ca/cmd/...
+# copia binarios de fabric-ca al path del sistema
+sudo cp $GOBIN/* /usr/local/bin/
+# Crea directorio de configuración del server Fabric-CA
+mkdir -p fabric-ca/server
+export FABRIC_CA_HOME=/home/fabric/fabric-ca/server
+## crear los archivo de configuración inicial del server con 2 instancias CA
+## Una para el CA de la organización y la otra para el TLSCA de la organización
+## No es necesario si ya se tiene los archivos de configuración previamente adecuados
+fabric-ca-server init -b admin:atc2020adm1n --cafiles /home/fabric/fabric-ca/server/tlsca.atc.catalyst.com/fabric-ca-server-config.yaml
+## modificar los valores de configuración en el archivo de configuración
+sudo systemctl start fabric-ca.service
+
+## Desde otra VM o incluso en la misma crear si no existe
+mkdir -p ~/fabric-ca/clients/ca/admin
+# establecer la variable de entorno
+export FABRIC_CA_CLIENT_HOME=~/fabric-ca/clients/ca/admin
+## Si no se tiene una configuración inicial para el cliente de fabric-ca, se generara automatica con los valores por defecto
+## Inscribir la identidad inicial, esto creará el directorio msp con los certificados de la identidad
+fabric-ca-client enroll -u http://admin:atc2020adm1n@atc.catalyst.com:7054
+## registrar otro admin para propositos de configuración desde otra maquina
+fabric-ca-client register -d --id.name admin@atc.catalyst.com --id.secret adm1nC4t4ly5t --id.type admin --id.affiliation atc.catalyst --id.attrs '"hf.Registrar.Roles=peer,client",hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert'
+## Registar el o los peer que se requieran
+fabric-ca-client register -d --id.name peer0.atc.catalyst.com --id.type peer --id.affiliation atc.catalyst --id.secret peer0ATCC4t4ly5t
+## Registrar el o los clientes que se requieran
+fabric-ca-client register -d --id.name user1@atc.catalyst.com --id.secret user1C4t4ly5t --id.affiliation atc.catalyst --id.type client
+## Desde la VM que va a ser peer, este comando genera el msp de dicho peer
+fabric-ca-client enroll -u http://peer0.atc.catalyst.com:peer0ATCC4t4ly5t@atc.catalyst.com:7054
+## lo mismo pasa para los usuarios cliente
+fabric-ca-client enroll -u http://user1@atc.catalyst.com:user1C4t4ly5t@atc.catalyst.com:7054
+## los mismo para el admin
+fabric-ca-client enroll -u http://admin@atc.catalyst.com:adm1nC4t4ly5t@atc.catalyst.com:7054
+
+
+### Los pasos anteriores de inscripción y registro de identidades incluida la inicial, se puede repetir para el CA de TLS
+## Ademas de cambiar el valor de la variables de entorno FABRIC_CA_CLIENT_HOME, se debe añadir en los comandos la directiva --caname <nombre_del_ca>
